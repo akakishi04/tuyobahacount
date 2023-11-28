e778d6854ebc09ec5e94b7077c57563e43da93f3
akakishi04 <akakishi04@gmail.com>
2023-11-28 16:43:34 +0900

    プロジェクト ファイルを追加します。

---
+++ tuyobahacount/ConfirmationDialog.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tuyobahacount
+{
+    /// <summary>
+    /// ConfirmationDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ConfirmationDialog : Window
+    {
+        public ConfirmationDialog()
+        {
+            InitializeComponent();
+        }
+        private void YesButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void NoButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
---
+++ tuyobahacount/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tuyobahacount
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.MouseLeftButtonDown += new MouseButtonEventHandler(MainWindow_MouseLeftButtonDown);
+            this.KeyDown += new KeyEventHandler(MainWindow_KeyDown);
+        }
+
+        private void MainWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // ウィンドウをドラッグで移動
+            this.DragMove();
+
+        }
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Esc キーが押されたかチェック
+            if (e.Key == Key.Escape)
+            {
+                // 確認ダイアログを表示
+                var customDialog = new ConfirmationDialog();
+                customDialog.Owner = this; // 'this' は現在のウィンドウを指します
+                
+             
+
+                // 「はい」が選択された場合、アプリケーションを終了
+                if ( customDialog.ShowDialog()==true)
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+        }
+
+        private void Nodrop(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
